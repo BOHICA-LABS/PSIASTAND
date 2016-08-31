1ad70188e23c154d2fac631fdd626ebc3dec3903
Joshua Maagdy <josh.magady@gmail.com>
2016-08-30 21:04:36 -0500

    misc: Removed Trailing White Space
    Removed the detected trailing white space from Get-GPOSettings Function file to align with the style guide.

--- tests/unit/GPO/RSOP/Get-GPOSettings.tests.ps1
+++ tests/unit/GPO/RSOP/Get-GPOSettings.tests.ps1
@@ -8,17 +8,17 @@ $PSVersion = $PSVersionTable.PSVersion.Major
 Describe "Get-GPOSettings PS: $PSVersion"{
   # Copy test data to TestDrive
   Copy-Item -Path "$Global:testData\Trackers\Sample04_Win2008R2MS.csv" -Destination 'TestDrive:\Sample_GPResultantSetOfPolicy_D2016-08-30T17.38.49.xml'
-  
+
   Context 'Strict Mode'{
     # Enable Strict Mode in Powershell
     Set-StrictMode -Version latest
-    
+
     It 'Should find results for the UserRightsAssignment Query'{
       # Pull the Count for the number of objects expected for the UserRightsAssignment Query
-      $Count = $((Get-GPOSettings -RsopXML 'TestDrive:\Sample_GPResultantSetOfPolicy_D2016-08-30T17.38.49.xml' -PassThru).QueryName | Where-Object {$_ -eq 'UserRightsAssignment'}).Count
+      $Count = $((Get-GPOSettings -RsopXML "$($TestDrive)\Sample_GPResultantSetOfPolicy_D2016-08-30T17.38.49.xml" -PassThru).QueryName | Where-Object {$_ -eq 'UserRightsAssignment'}).Count
       $Count | Should be 43
     }
-    
+
     It 'Should find results for the SecurityOptions Query'{
       # Pull the Count for the number of objects expected for the SecurityOptions Query
       $Count = $((Get-GPOSettings -RsopXML 'TestDrive:\Sample_GPResultantSetOfPolicy_D2016-08-30T17.38.49.xml' -PassThru).QueryName | Where-Object {$_ -eq 'SecurityOptions'}).Count
@@ -30,42 +30,42 @@ Describe "Get-GPOSettings PS: $PSVersion"{
       $Count = $((Get-GPOSettings -RsopXML 'TestDrive:\Sample_GPResultantSetOfPolicy_D2016-08-30T17.38.49.xml' -PassThru).QueryName | Where-Object {$_ -eq 'Account'}).Count
       $Count | Should be 9
     }
-    
+
     It 'Should find results for the Audit Query'{
       # Pull the Count for the number of objects expected for the Audit Query
       $Count = $((Get-GPOSettings -RsopXML 'TestDrive:\Sample_GPResultantSetOfPolicy_D2016-08-30T17.38.49.xml' -PassThru).QueryName | Where-Object {$_ -eq 'Audit'}).Count
       $Count | Should be 8
     }
-    
+
     It 'Should find results for the EventLog Query'{
       # Pull the Count for the number of objects expected for the EventLog Query
       $Count = $((Get-GPOSettings -RsopXML 'TestDrive:\Sample_GPResultantSetOfPolicy_D2016-08-30T17.38.49.xml' -PassThru).QueryName | Where-Object {$_ -eq 'EventLog'}).Count
       $Count | Should be 9
     }
-    
+
     It 'Should find results for the RestrictedGroups Query'{
       # Pull the Count for the number of objects expected for the RestrictedGroups Query
       $Count = $((Get-GPOSettings -RsopXML 'TestDrive:\Sample_GPResultantSetOfPolicy_D2016-08-30T17.38.49.xml' -PassThru).QueryName | Where-Object {$_ -eq 'RestrictedGroups'}).Count
       $Count | Should be 2
     }
-    
+
     It 'Should find results for the SystemServices Query'{
       # Pull the Count for the number of objects expected for the SystemServices Query
       $Count = $((Get-GPOSettings -RsopXML 'TestDrive:\Sample_GPResultantSetOfPolicy_D2016-08-30T17.38.49.xml' -PassThru).QueryName | Where-Object {$_ -eq 'SystemServices'}).Count
       $Count | Should be 12
     }
-    
+
     It 'Should find results for the File Query'{
       # Pull the Count for the number of objects expected for the File Query
       $Count = $((Get-GPOSettings -RsopXML 'TestDrive:\Sample_GPResultantSetOfPolicy_D2016-08-30T17.38.49.xml' -PassThru).QueryName | Where-Object {$_ -eq 'File'}).Count
       $Count | Should be 11
     }
-    
+
     It 'Should find results for the Registry Query'{
       # Pull the Count for the number of objects expected for the Registry Query
       $Count = $((Get-GPOSettings -RsopXML 'TestDrive:\Sample_GPResultantSetOfPolicy_D2016-08-30T17.38.49.xml' -PassThru).QueryName | Where-Object {$_ -eq 'Registry'}).Count
       $Count | Should be 11
     }
-    
+
   }
 }
