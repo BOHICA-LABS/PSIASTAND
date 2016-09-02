35022509bfa168259d5c4bc5fe9f13730c7e8e07
Jared Richards <jaredbrichards@gmail.com>
2016-09-02 11:24:04 -0500

    feat: Export-tracker Development in progress

---
+++ functions/stigviewer/Export-tracker.ps1
@@ -0,0 +1,41 @@
+﻿function Export-Tracker {
+<#
+.SYNOPSIS
+This function exports a ckl object to a csv tracker  file
+
+.PARAMETER Object
+    ckl object loaded  from import-ckl
+
+.PARAMETER exportlocation
+    export location for the file
+
+.EXAMPLE
+
+.LINK
+
+.VERSION
+1.0.0 (09.02.2016)
+    Initial release
+
+#>
+
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$false)]
+        [object]$object,
+        [Parameter(Mandatory=$false)]
+        [string]$exportlocation
+    )
+
+    foreach($row in $object) {
+        
+        write-host $row
+    }
+
+}
+
+Export-Tracker -object $ckl -exportlocation .\test.csv
+
+#$file = Get-item .\tests\data\CKL\CKLv2\sampleV2.ckl
+#$xml = import-xml -fileobj $file
+#$ckl = import-ckl -doc $xml
