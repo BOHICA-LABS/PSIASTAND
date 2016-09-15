4d1d1b96fe5c4377279b5a66ab8d57d1b9d4a7b1
Joshua Magady <Joshua Magady>
2016-09-15 13:18:21 -0500

    wip: Working on tests for Activate-Watcher
    During testing development found that the current method could not keep up with a large amount of writes

--- tests/unit/File/Monitor/Activate-Watcher.tests.ps1
+++ tests/unit/File/Monitor/Activate-Watcher.tests.ps1
@@ -1,1 +1,19 @@
-﻿
+<#
+    .Version
+    1.0.0.0
+#>
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+
+
+$moduleName = 'PSIASTAND'
+$PSVersion = $PSVersionTable.PSVersion.Major
+
+Describe "Activate-Watcher PS: $PSVersion"{
+  Context 'Strict Mode'{
+    # Enable Strict Mode in Powershell
+    Set-StrictMode -Version latest
+    
+  }
+}
