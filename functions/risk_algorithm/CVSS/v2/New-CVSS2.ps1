a51b2645a04cbe6516f7df28a70955923cc08a24
Joshua Magady <Joshua Magady>
2016-09-23 22:55:09 -0500

    wip: Continued populating the Metric Weights

--- functions/risk_algorithm/CVSS/v2/New-CVSS2.ps1
+++ functions/risk_algorithm/CVSS/v2/New-CVSS2.ps1
@@ -35,9 +35,9 @@
   # values. It does not check that a metric is specified more than once and it does not check that all base
   # metrics are present. These checks need to be performed separately.
   
-  $CVSS.vectorStringRegex_20 = New-Object System.Text.RegularExpressions.Regex '^CVSS:2\.0\/((AV:[LAN]|AC:[HML]|AU:[MSN]|C:[NPC]|I:[NPC]|A:[NPC]|E:([UF]|POC|ND)|RL:([WU]|OF|TF|ND)|RC:([C]|UC|UR|ND)|CDP:([NLH]|LM|MH|ND)|TD:([NLMH]|ND)|CR:([LMH]|ND)|IR:([LMH]|ND)|AR:([LMH]|ND))\/)*(AV:[LAN]|AC:[HML]|AU:[MSN]|C:[NPC]|I:[NPC]|A:[NPC]|E:([UF]|POC|ND)|RL:([WU]|OF|TF|ND)|RC:([C]|UC|UR|ND)|CDP:([NLH]|LM|MH|ND)|TD:([NLMH]|ND)|CR:([LMH]|ND)|IR:([LMH]|ND)|AR:([LMH]|ND))$', 'IgnoreCase'
+  $CVSS.vectorStringRegex_20 = New-Object System.Text.RegularExpressions.Regex '^CVSS:2\.0\/((AV:[LAN]|AC:[HML]|AU:[MSN]|C:[NPC]|I:[NPC]|A:[NPC]|E:([UFH]|POC|ND)|RL:([WU]|OF|TF|ND)|RC:([C]|UC|UR|ND)|CDP:([NLH]|LM|MH|ND)|TD:([NLMH]|ND)|CR:([LMH]|ND)|IR:([LMH]|ND)|AR:([LMH]|ND))\/)*(AV:[LAN]|AC:[HML]|AU:[MSN]|C:[NPC]|I:[NPC]|A:[NPC]|E:([UFH]|POC|ND)|RL:([WU]|OF|TF|ND)|RC:([C]|UC|UR|ND)|CDP:([NLH]|LM|MH|ND)|TD:([NLMH]|ND)|CR:([LMH]|ND)|IR:([LMH]|ND)|AR:([LMH]|ND))$', 'IgnoreCase'
   
-  # Associative arrays mapping each metric value to the constant defined in the CVSS scoring formula in the CVSS v3.0
+  # Associative arrays mapping each metric value to the constant defined in the CVSS scoring formula in the CVSS v2.0
   # specification.
   
   $CVSS.Weight = @{
@@ -46,6 +46,11 @@
     AU = @{ M = 0.45; S = 0.56; N = 0.704; };
     CIA = @{ N = 0.0; P = 0.275; C = 0.660; }; # C, I and A have the same weights
 
+    E = @{ U = 0.85; POC = 0.9; F = 0.95; H = 1.00; ND = 1.00; };
+    RL = @{ OF = 0.87; TF = 0.90; W = 0.95; ND = 1.00; };
+    RC = @{ UC = 0.90; UR = 0.95; C = 1.00; ND = 1.00; };
     
+    CDP = @{ N = 0; L = 0.1; LM = 0.3; MH = 0.4; H = 0.5; ND = 0; };
+    TD = @{ N = 0; L = 0.25; M = 0.75; H = 1.00; ND = 1.00; };
   }
 }
