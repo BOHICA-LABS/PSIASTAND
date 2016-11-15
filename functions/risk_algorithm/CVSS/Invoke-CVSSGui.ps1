4cf6fa36811ad16f9cc83db43d37d80830097523
Joshua Magady <Joshua Magady>
2016-11-14 23:21:49 -0600

    wip: built CLI gui
    Started building the CLI gui

---
+++ functions/risk_algorithm/CVSS/Invoke-CVSSGui.ps1
@@ -0,0 +1,34 @@
+﻿function Invoke-CVSSGui
+{
+  <#
+      .SYNOPSIS
+      Launches the CLI for assigning CVSS Scores
+
+      .PARAMETER 
+
+      .PARAMETER 
+
+      .PARAMETER
+
+      .PARAMETER 
+
+
+      .EXAMPLE
+
+
+      .LINK
+
+      .VERSION
+      1.0.0 (11.14.2016)
+        -Intial Release
+
+  #>
+  [CmdletBinding()]
+  Param
+  (
+    [int]$version,
+    [string]$mapper,
+    [string]$risks
+  )
+  
+}
