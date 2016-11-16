beb8b029408b8011602aba47bd6d1d531e4928af
Joshua Magady <Joshua Magady>
2016-11-16 15:19:48 -0600

    wip: Built a Seperate Risk Mapper for CVSS

---
+++ functions/risk_algorithm/CVSS/Import-RiskAssessment.ps1
@@ -0,0 +1,57 @@
+﻿function Import-RiskAssessment
+{
+  <#
+      .SYNOPSIS
+      Launches the CLI for assigning CVSS Scores
+
+      .PARAMETER 
+
+      .PARAMETER 
+
+      .PARAMETER
+
+      .PARAMETER 
+
+
+      .EXAMPLE
+
+
+      .LINK
+
+      .VERSION
+      1.0.0 (11.14.2016)
+        -Intial Release
+
+  #>
+  
+  [CmdletBinding()]
+  param
+  (
+    [String]
+    $risk = $(Throw "No RISK Path provided"),
+  
+    [String]
+    $map = $(Throw "No MAP provided"),
+    
+    [String]
+    $output = $(Throw "No Output folder provided"),
+    
+    [String]
+    $name = $(Throw "No Name provided")
+  )
+  
+  # Test to see if the path to the risk report exists
+  if(!(Test-Path -Path $risk)){Throw "Risk path does not exist"} 
+  
+  # Test to see if the path to the map report exists
+  if(!(Test-Path -Path $map)){Throw "Map path does not exist"}
+
+  # Import Risk Elements
+  $riskelements = Import-XLSX -Path $risk
+  
+  # Import mapper
+  $riskmap = Import-XLSX -Path $map
+  
+  # Select the ID from the Risk Map
+  $riskmapnamelist = $(($riskmap | Select ID).ID)
+}
